2e8c49d84ee1329149785fc2fb768ebdfe616279
Meunier_David <meunier.dev@gmail.com>
2023-01-27 16:36:14 +0100

    Et non !

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello Git, i'm coming, my name is David !")
+
+
+def afficher_Colonnes(rows = 5):
+    for i in range(rows, -1, -1):
+        
+        for line in range(0, i+1):
+            print(line, end='')
+        print("\r")
+
+afficher_Colonnes(5)
